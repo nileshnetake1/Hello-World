f24856681d0a3e221645459f25f5a3f9d3e30596
nnilesh <nilesh.netake@webonise.com>
2013-04-03 19:01:46 +0530

    My php part

--- interfaces.php
+++ interfaces.php
@@ -1,11 +1,5 @@
 <?php
-/**
- * Created by JetBrains PhpStorm.
- * User: webonise
- * Date: 3/4/13
- * Time: 5:39 PM
- * To change this template use File | Settings | File Templates.
- */
+
 interface Inter{
 
     const a="This is constant value";
